cb406df613ce3192eefb097a6c3dd3f83e6d262a
mohElhakiem <moh888fy@gmail.com>
2025-06-11 00:24:33 +0300

    Add files via upload

---
+++ part1.py
@@ -0,0 +1,9 @@
+contacts = {"mohamed":"01235467866","aly":"01133322668","salah":"01077768221"}
+
+print(contacts.keys())
+
+contacts["karim"] = "01554466333"
+contacts["emad"] = "01115646802"
+contacts["sayed"] = "01234438891"
+
+print(contacts.keys())
---
+++ part2.py
@@ -0,0 +1,18 @@
+
+
+t= {"omar":[48,50,43],"ahmed":[36,45,46],"yasser":[40,42,38]}
+
+print(t)
+
+avg_of_omar = sum(t["omar"]) /3
+
+avg_of_ahmed = sum(t["ahmed"]) /3
+
+avg_of_yasser = sum(t["yasser"]) /3
+
+print(avg_of_omar)
+
+print(avg_of_ahmed)
+
+print(avg_of_yasser)
+
